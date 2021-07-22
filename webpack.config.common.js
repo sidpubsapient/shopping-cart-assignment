1d1c581b68db0a973a9acd1d462df1d283625a15
Siddharth Yadav <sidyadav1@WKMZT158B569.global.publicisgroupe.net>
2021-07-22 14:47:33 +0530

    setting up CI via gihtbu actions

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -2,7 +2,6 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
 
 module.exports = {
     entry: "./src/index.js",
