687d489760f8d14dc2664e6a717991f904433c5e
Siddharth Yadav <sidyadav1@WKMZT158B569.global.publicisgroupe.net>
2021-08-05 15:55:47 +0530

    committing carousel component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
-// import { Redirect, Route, Switch } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import { GlobalStyles } from "./styles/global";
-// import Home from "./modules/Home";
-// import Register from "./modules/Register";
-// import Products from "./modules/PLP";
-// import SignIn from "./modules/SignIn";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Home from "./modules/Home";
 
 export const App = function () {
     return (
         <>
             <Header />
-            <main></main>
+            <main>
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Redirect to="/" />
+                    </Switch>
+                </div>
+            </main>
             {/* <Footer /> */}
         </>
     );
